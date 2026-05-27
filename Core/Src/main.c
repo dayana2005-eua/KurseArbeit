ffa342e3855bf40c835545e596ca4f93809ab0f0
supspitdamn <pyatkov.dim@gmail.com>
2026-05-28 00:03:05 +0600

    Предположительно хорошее нововведение в робота

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,10 +3,11 @@
 #include "sensors.h"
 #include "motor.h"
 
-volatile uint8_t tim2_flag = 0;
-
-const int16_t HIGH_SPEED = 450; 
-const int16_t MOTOR_TRIM = -30; 
+// ================= НАСТРОЙКА ПАРАМЕТРОВ РОБОТА =================
+#define HIGH_SPEED   450   // Маршевая скорость бега вперед по прямой
+#define MOTOR_TRIM   (-30) // Аппаратный баланс левого мотора
+#define INNER_SPEED  0     // Скорость внутреннего колеса при повороте
+// ===============================================================
 
 int main(void)
 {
@@ -14,49 +15,41 @@ int main(void)
     SysTick_init();
     button_interrupt_init();
     GPIO_init();
-    TIMER2_init(); // Запуск ШИМ и прерываний таймера
+    TIMER2_init(); 
 
     uint8_t left = 0;
     uint8_t right = 0;
 
     while(1)
     {
-        // Процессор физически засыпает и ждет прерывания таймера
         while (get_tim2_flag() == 0)
         {
             __WFI(); 
         }
-
-        set_tim2_flag(0); // Сбрасываем флаг и выполняем шаг управления строго раз в 1 мс
+        set_tim2_flag(0); 
 
         if (get_button_permission())
         {
-            // Опрос датчиков выполняется в main
             left  = read_left_sensor();
             right = read_right_sensor();
 
-            // Алгоритм "вилки" (линия удерживается между датчиками)
             if (left == 0 && right == 0)
             {
-                // Линия по центру -> едем прямо
                 left_motor(HIGH_SPEED + MOTOR_TRIM);
                 right_motor(HIGH_SPEED);
             }
             else if (left == 1 && right == 0)
             {
-                // Линия слева -> доворачиваем влево
                 left_motor(HIGH_SPEED + MOTOR_TRIM);
-                right_motor(0); 
+                right_motor(INNER_SPEED); 
             }
             else if (right == 1 && left == 0)
             {
-                // Линия справа -> доворачиваем вправо
-                left_motor(0 + MOTOR_TRIM); 
+                left_motor(INNER_SPEED + MOTOR_TRIM); 
                 right_motor(HIGH_SPEED);
             }
             else
             {
-                // Перекресток или полная потеря линии -> стоп
                 left_motor(0);
                 right_motor(0);
             }
